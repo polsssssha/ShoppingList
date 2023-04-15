bd33705c7a889d111ffc024ea8e82e87b2bd9646
polsssha <nusty11102003@gmail.com>
2023-04-15 23:22:46 +0500

    аоао

--- src/main/java/com/example/lab_9/Controllers/ShoppingController.java
+++ src/main/java/com/example/lab_9/Controllers/ShoppingController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 import java.util.Map;
 @RequestMapping("/api/products")
+@RestController
 public class ShoppingController {
 
     @Autowired
